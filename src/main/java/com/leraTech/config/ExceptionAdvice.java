9e38be2c2a983b3323f44bc1f8833292616727ef
leelakrishnacce2018 <leelakrishnacce2018@gmail.com>
2019-02-03 18:53:20 +0530

    spring boot mysql jwt authenication template

---
+++ src/main/java/com/leraTech/config/ExceptionAdvice.java
@@ -0,0 +1,9 @@
+package com.leraTech.config;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+
+}
---
+++ src/main/java/com/leraTech/service/UserService.java
@@ -0,0 +1,19 @@
+package com.leraTech.service;
+
+import java.util.List;
+
+import com.leraTech.model.User;
+import com.leraTech.model.UserDto;
+
+public interface UserService {
+
+    User save(UserDto user);
+    List<User> findAll();
+    void delete(int id);
+
+    User findOne(String username);
+
+    User findById(int id);
+
+    UserDto update(UserDto userDto);
+}
